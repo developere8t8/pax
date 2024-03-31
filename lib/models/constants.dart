5beba65d490f5aae2476a3250665d62dec87a537
developer8t8 <117169930+developere8t8@users.noreply.github.com>
2024-04-01 03:42:30 +0500

    Update constants.dart

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -1,13 +1,9 @@
 ///
 ///TOKENS FOR COUNTRIES & STATE CALL API
 //this token is used to get access token
-const String stateApiToken = 'r8XdMDEus-BEi7kA7h7n7pHfDv5kEZ8fks7T_EVsBdePhT5NmI1NInxa8kGShkBYkAM';
+const String stateApiToken = 'xxxxxxxx';
 
-// //tis token will be passed in every api cal/
-// const String accessToken =
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJhcnNhbG4wMDBAZ21haWwuY29tIiwiYXBpX3Rva2VuIjoicjhYZE1ERXVzLUJFaTdrQTdoN243cEhmRHY1a0VaOGZrczdUX0VWc0JkZVBoVDVObUkxTklueGE4a0dTaGtCWWtBTSJ9LCJleHAiOjE2NjgyODA0MzB9.bhTxcYaFurVKjEuQyD5LQmsMA48i7_MeE2A3sK2UHOo';
-//user email for countries token
-const String email = 'arsaln000@gmail.com';
+  
 
 ///
 ///Stripe data
@@ -18,14 +14,14 @@ const String baseUrl = 'https://api.stripe.com';
 //strip secret key
 
 const String stripSecretKey =
-    'sk_test_51LSzM5K0fDYsd9eginxDXl9Ja3eQKdzlkTGWL9hd9Z9kTmIMOaAPQ4fVZi1XC8m7dj76QW3hLIXMNkyjPnoEoEDo008BnHKPJM';
+    'xxxxxxxx';
 
 //dacast base url for video streamin
 const String dacastApiBaseUrl = 'https://developer.dacast.com';
 
 //dacast api key
 
-const String dacastApiKey = '1668551071Cn76eB9DDursfxWHWA0cmc7G9mA9T0Cd';
+const String dacastApiKey = '1xxxxxxxxxxxx';
 
 //dacast playback api url
 
